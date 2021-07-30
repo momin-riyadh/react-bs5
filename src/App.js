5fbfbadcf409aeeb7ad8e37f8df855269d4ae3f3
momin-riyadh <mominriyadh@gmail.com>
2021-07-30 11:09:16 +0600

    added folder stracture

--- src/App.js
+++ src/App.js
@@ -19,11 +19,14 @@ function App() {
                         <img width="60px" className="img-fluid" src={Logo} alt=""/>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav" className="ml-auto">
-                        <Nav className="me-auto">
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <Nav.Link href="#link">About Us</Nav.Link>
+                            <Nav.Link href="#link">Services</Nav.Link>
+                            <Nav.Link href="#link">Products</Nav.Link>
+                            <Nav.Link href="#link">Contact</Nav.Link>
+                            <NavDropdown title="Admin" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
