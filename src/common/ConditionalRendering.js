c9216a6357b3febf8f1f683e9d787c1fcea9fd11
momin-riyadh <mominriyadh@gmail.com>
2021-08-03 10:44:15 +0600

    fix and deploy

--- src/common/ConditionalRendering.js
+++ src/common/ConditionalRendering.js
@@ -6,7 +6,20 @@ class ConditionalRendering extends React.Component {
         super();
         this.state = {
             isLoggedIn: true,
+            count: 0,
         }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    handleClick() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 2
+            }
+
+        })
     }
 
     render() {
@@ -16,11 +29,17 @@ class ConditionalRendering extends React.Component {
         } else {
             wordDisplay = "Out"
         }
+
         return (
             <div className="card mb-5">
                 <div className="card-body">
                     <p style={{color: "#000000", fontSize: "20px", margin: 0}}>Now you are <em>Logged</em> {wordDisplay}
                     </p>
+                </div>
+
+                <div className="card-footer text-center">
+                    <h1 style={{color: "coral"}}>{this.state.count}</h1>
+                    <button className="btn btn-lg btn-outline-danger" onClick={this.handleClick}>Update!</button>
                 </div>
             </div>
 
