f4fd53c91a94c77e069fcf2abaa7fb47e18d1c97
momin-riyadh <mominriyadh@gmail.com>
2021-08-16 21:16:13 +0600

    fix and deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/main.css';
-import './assets/css/index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
